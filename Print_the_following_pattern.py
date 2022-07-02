c2e310f8589e97a4f960866800eafa1027f042d0
21A91A05H4 <97424546+21A91A05H4@users.noreply.github.com>
2022-07-02 23:34:57 +0530

    n=int(input()) alph="ABCDEFGHIJKLMNOPQRSTUVWXYZ" for i in range(n):     for j in range(n-i-1):         print(' ',end='')     for j in range(i+1):         print(alph[j],end='')     for j in range(i,0,-1):         print(alph[j-1],end='')     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,5 +1,10 @@
 n=int(input())
+alph="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for i in range(n):
-    for j in range(i+1,n+1):
-        print(j,end=' ')
+    for j in range(n-i-1):
+        print(' ',end='')
+    for j in range(i+1):
+        print(alph[j],end='')
+    for j in range(i,0,-1):
+        print(alph[j-1],end='')
     print()
